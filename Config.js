59586159971350616a6fcccbc542f32f6b5ddb4a
entbudc-cmyk <ent.budc@cpird.in.th>
2025-09-10 16:34:17 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,8 +1,9 @@
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbyiR7iAEQJ10ZiMKA6qaKV_SWS9OcIGX_VNwhyZ1veJoZc5NN5an31oWbFqEvac52YI/exec';
+    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbxhYSqmSMZkHoRTRDvs1B4lAdSI4EBVHN3PFdNpD00lyOvCuw_cYb-NVk_k4_aLPvR-/exec';
     const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwbuIiJWIJOyXDRmCjVjAO3IvbRD7YMKMXxmvtjQhM9PRSRz3tYWCiUQgn-bqdzVhQi/exec';
 //LIFF_ID
     const LIFF_ID = '2008087990-j83w8AAz'; // LIFF ID Checkin
     const LIFF_HISTORY = '2008087990-jMnw3VVM'; // LIFF ID history
     const LIFF_REGISTER = '2008087990-ZozKjoo1'; // LIFF ID register
+
 
